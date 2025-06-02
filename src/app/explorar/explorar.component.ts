1f629a0153c39e448ac09b682f42da7b307311b9
André Schultz <169122435+andreschultzz@users.noreply.github.com>
2025-06-02 13:06:41 -0300

    02/06

---
+++ src/app/explorar/explorar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-explorar',
+  imports: [],
+  templateUrl: './explorar.component.html',
+  styleUrl: './explorar.component.css'
+})
+export class ExplorarComponent {
+
+}

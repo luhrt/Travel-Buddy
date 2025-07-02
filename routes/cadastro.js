033b56ef185ae9934b41ff7e09c47ff788112f3f
AndreSchultz <andreschultz2005@gmail.com>
2025-07-01 23:07:34 -0300

    testes aplicados

--- routes/cadastro.js
+++ routes/cadastro.js
@@ -15,9 +15,10 @@ router.post('/cadastrar',async (req, res) => {
         const [result] = await global.conexao.query('INSERT INTO user (user_email, user_name, user_password) VALUES (?, ?, ?)', [email, nome, senha]);
 
         const userId = result.insertId;
+        console.log(`✅ Cadastro realizado com sucesso! Usuário '${nome}' (ID: ${userId}) inserido no banco de dados.`);
         res.redirect(`/profile/${userId}`);
     } catch(err) {
-        console.error(err);
+        console.error("❌ Erro ao tentar cadastrar usuário:", err);
         res.status(500).send("Erro ao cadastrar destino.");
     }
 })

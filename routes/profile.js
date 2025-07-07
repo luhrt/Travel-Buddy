07dd9d183a9d31d8d118201198c73488ccc8f772
AndreSchultz <andreschultz2005@gmail.com>
2025-07-07 08:11:01 -0300

    profile page

--- routes/profile.js
+++ routes/profile.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 // BOMBA
-router.get('/',function(req, res, next){
-    res.render('profile', {title: 'TravelBuddy'});
+router.get('/', function(req, res, next) {
+    res.render('profile', {
+        title: 'TravelBuddy',
+        user: { user_name: 'Nome de Teste' } // Adiciona um objeto user para o EJS
+    });
 });
 
-module.exports = router
+module.exports = router;
